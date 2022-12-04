73ba73f5df1193725a8592e1afd69497a93e6c6c
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-04 20:59:21 +0000

    add: created django project app & base apps

---
+++ seaside_sewing/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for seaside_sewing project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seaside_sewing.settings')
+
+application = get_asgi_application()
