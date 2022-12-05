1117b19d38b27129d2e6da7a6713ff7dda3dbd1e
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-05 17:13:59 +0000

    add: apps created to the installed_apps list

--- seaside_sewing/settings.py
+++ seaside_sewing/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bag',
+    'checkout',
+    'home',
+    'products',
+    'profiles',
+    
 ]
 
 MIDDLEWARE = [
