350d7d84c55cecd6b4da6a9f07058e1a865d3371
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-10 23:43:11 +0000

    add: settings to wire up the home page

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """
+    View that returns the home page
+    """
+    return render(request, "home/index.html")
--- seaside_sewing/settings.py
+++ seaside_sewing/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -63,7 +64,10 @@ ROOT_URLCONF = 'seaside_sewing.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
