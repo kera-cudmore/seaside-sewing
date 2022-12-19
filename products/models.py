756dd74ebe3c98281bf523010488f1d97323005d
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-19 21:06:39 +0000

    add: category & products models & initial migration

--- products/models.py
+++ products/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(
+        max_length=254
+    )
+    friendly_name = models.CharField(
+        max_length=254,
+        blank=True,
+        null=True,
+    )
+
+    def __Str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        'Category',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+    sku = models.CharField(
+        max_length=254,
+        null=True,
+        blank=True
+        )
+    name = models.CharField(
+        max_length=254
+        )
+    description = models.TextField(
+    )
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2
+        )
+    rating = models.PositiveSmallIntegerField(
+        default=0,
+        null=True,
+        blank=True
+    )
+    image_url = models.URLField(
+        max_length=1024,
+        null=True,
+        blank=True
+        )
+    image = models.ImageField(
+        null=True,
+        blank=True
+        )
+
+    def __str__(self):
+        return self.name
