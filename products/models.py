49450368cc91f2650829dc4945ffca74fb44bae0
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-19 21:07:48 +0000

    update: remove ratings & add colours and quantity fields & migrated

--- products/models.py
+++ products/models.py
@@ -32,31 +32,33 @@ class Product(models.Model):
     sku = models.CharField(
         max_length=254,
         null=True,
-        blank=True
-        )
+        blank=True,
+    )
     name = models.CharField(
-        max_length=254
-        )
+        max_length=254,
+    )
     description = models.TextField(
     )
     price = models.DecimalField(
         max_digits=6,
-        decimal_places=2
-        )
-    rating = models.PositiveSmallIntegerField(
-        default=0,
-        null=True,
-        blank=True
+        decimal_places=2,
     )
     image_url = models.URLField(
         max_length=1024,
         null=True,
-        blank=True
-        )
+        blank=True,
+    )
     image = models.ImageField(
         null=True,
-        blank=True
-        )
+        blank=True,
+    )
+    quantity = models.PositiveSmallIntegerField(
+    )
+    colour = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+    )
 
     def __str__(self):
         return self.name
