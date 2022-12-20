8c518509a6036582e92d742e8a465e54bf37432c
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-20 18:03:42 +0000

    update: media and static settings

--- seaside_sewing/settings.py
+++ seaside_sewing/settings.py
@@ -166,7 +166,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR, 'static']
 
 # Media files
 MEDIA_URL = '/media/'
