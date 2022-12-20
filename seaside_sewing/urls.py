f1fcfabaac6432c41059c96e647b7de2492e9a30
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-20 19:21:07 +0000

    add: imported settings and static and added static to the end of urlpatterns

--- seaside_sewing/urls.py
+++ seaside_sewing/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,4 @@ urlpatterns = [
     # path('bag/', include('bag.urls')),
     # path('profiles/', include('profiles.urls')),
     # path('checkout/', include('checkout.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
