e249ca304312e4ac05f7c5ccfa231deea37ea785
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-25 14:19:22 +0000

    add: delivery page info & fixed spelling error in urls.py

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('privacy', views.privacy, name='privacy'),
     path('terms', views.terms, name='terms'),
-    path('delivery', view.delivery, name='delivery'),
+    path('delivery', views.delivery, name='delivery'),
 ]
