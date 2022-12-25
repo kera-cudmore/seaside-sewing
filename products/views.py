04fa138a96ead0bab6a8359ba9fd055565f01a17
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-25 18:38:06 +0000

    add: js logic for the sort by select field & a back to top button

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
+from django.db.models.functions import Lower
 
 
 def all_products(request):
