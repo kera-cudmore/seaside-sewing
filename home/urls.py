0c58d9886fe65732fa2dd1745f3fe1efe95e2ae7
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-25 00:02:50 +0000

    add: created pages for the privacy, terms & delivery policies - added the url paths & created views

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('privacy', views.privacy, name='privacy'),
+    path('terms', views.terms, name='terms'),
+    path('delivery', view.delivery, name='delivery'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,24 @@ def index(request):
     View that returns the home page
     """
     return render(request, "home/index.html")
+
+
+def privacy(request):
+    """
+    View that returns the privacy policy page
+    """
+    return render(request, "home/privacy.html")
+
+
+def terms(request):
+    """
+    View that returns the terms and conditions page
+    """
+    return render(request, "home/terms.html")
+
+
+def delivery(request):
+    """
+    View that returns the delivery policy page
+    """
+    return render(request, "home/delivery.html")
