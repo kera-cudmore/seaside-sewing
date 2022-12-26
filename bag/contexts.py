19b6bd2bdb5a5d3e0a0ddc9b656529a875326538
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-26 16:00:35 +0000

    fix:  error in quantity for bag_items.append

--- bag/contexts.py
+++ bag/contexts.py
@@ -17,7 +17,7 @@ def bag_contents(request):
         product_count += quantity
         bag_items.append({
             'item_id': item_id,
-            'quantity': item_data,
+            'quantity': quantity,
             'product': product,
         })
 
