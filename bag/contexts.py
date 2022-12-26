7a0b85d1b6f51d773c59973071b806951878d0f5
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-26 15:59:37 +0000

    add: logic to for adding items & quantity to display in bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from decimal import Decimal
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def bag_contents(request):
@@ -7,6 +9,17 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': item_data,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total + Decimal(settings.STANDARD_DELIVERY)
