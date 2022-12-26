baa8740f8e40b9a300fc8ec4a1eb4079b49a7aa2
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-26 15:25:54 +0000

    add: imports

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from products.models import Product
 
 
 def view_bag(request):
