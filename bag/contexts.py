3a936c22b6a085cce4e4e31c2ec3f5e5758a99a3
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-26 13:47:27 +0000

    add: variables for working out free delivery & totals, passed these into the context to be available site-wide

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,7 +1,30 @@
+from django.conf import settings
+from decimal import Decimal
 
 
 def bag_contents(request):
 
-    context = {}
+    bag_items = []
+    total = 0
+    product_count = 0
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total + Decimal(STANDARD_DELIVERY)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+
+    grand_total = delivery + total
+
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'grand_total': grand_total,
+    }
 
     return context
