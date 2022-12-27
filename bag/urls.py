8949515deea70c66952e4739f469358c3bbef99b
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-27 01:59:31 +0000

    add: paths for adjusting and removing from bag

--- bag/urls.py
+++ bag/urls.py
@@ -3,5 +3,18 @@ from . import views
 
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
-    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
+    path(
+        'add/<int:item_id>/',
+        views.add_to_bag,
+        name='add_to_bag'
+    ),
+    path(
+        'adjust/<int:item_id>/',
+        views.adjust_bag,
+        name='adjust_bag'
+    ),
+    path(
+        'remove/<int:item_id>/',
+        views.remove_from_bag,
+        name='remove_from_bag'),
 ]
