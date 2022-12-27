d7d0b42b0e7eb439b580a6aab0a68ae7cfc9d461
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-27 01:58:01 +0000

    update: delivery variable to use the standard delivery set in settings.py

--- bag/contexts.py
+++ bag/contexts.py
@@ -22,7 +22,7 @@ def bag_contents(request):
         })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total + Decimal(settings.STANDARD_DELIVERY)
+        delivery = Decimal(settings.STANDARD_DELIVERY)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
