784b9fd200747b65c65aa40ceef73d235d152725
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-27 20:42:00 +0000

    refactor: format code

--- bag/urls.py
+++ bag/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_bag, name='view_bag'),
+    path(
+        '',
+        views.view_bag,
+        name='view_bag'
+    ),
     path(
         'add/<int:item_id>/',
         views.add_to_bag,
