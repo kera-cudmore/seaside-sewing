e9a5e1f182f80b111dbcd50fee979fa600ac959a
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-29 19:16:23 +0000

    add: error handling when inputting card no

--- checkout/static/checkout/js/stripe-elements.js
+++ checkout/static/checkout/js/stripe-elements.js
@@ -31,3 +31,18 @@ var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
 
+// Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+        <span class="icon" role="alert">
+        <i class="fas fa-times"></i>
+        </span>
+        <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
