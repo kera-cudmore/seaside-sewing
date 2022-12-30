2aec3ad2b9b4e85e44bc4bb0b24d47c9c841c3fe
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-30 13:16:38 +0000

    add: set up the stripe public and secret keys in environment

--- checkout/views.py
+++ checkout/views.py
@@ -9,6 +9,10 @@ import stripe
 
 
 def checkout(request):
+
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "Your bag is currently empty")
@@ -17,13 +21,23 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'The Stripe public key is missing. \
+            Please check it is set in the environment.')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51MKQDtCsSyXX0JFXkkAY7q0llbSBIRF3HFp1Wx8AAn9eVx7ePUlZdJ8rsBB6QnI3HdiJOavWxeDFuNii5nKmrHb700HvWOo6gy',
-        'client_secret': 'test-client-secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
