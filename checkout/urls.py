8f51a6126546359264346d20c06e8accdc9b11e9
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-30 15:06:31 +0000

    fix: spelling error & code refactor

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.checkout, name='checkout'),
-    path('checkout-success/<order_number>', views.checkout-success, name='checkout-success'),
+    path(
+        '',
+        views.checkout,
+        name='checkout'
+    ),
+    path(
+        'checkout-success/<order_number>',
+        views.checkout_success,
+        name='checkout-success'
+    ),
 ]
