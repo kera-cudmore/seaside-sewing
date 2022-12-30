5b8ceb5e0fd7914854119b66afb68a104d33a79b
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-30 11:54:09 +0000

    add: stripe import & settings

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
+
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 
 def checkout(request):
@@ -8,6 +13,10 @@ def checkout(request):
     if not bag:
         messages.error(request, "Your bag is currently empty")
         return redirect(reverse('products'))
+
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
