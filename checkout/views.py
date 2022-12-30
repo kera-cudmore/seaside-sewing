84c379c07ea29fc544a5bd9db89514d225b267f4
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-30 11:51:09 +0000

    add: bag contexts import to allow access to total

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MKQDtCsSyXX0JFXkkAY7q0llbSBIRF3HFp1Wx8AAn9eVx7ePUlZdJ8rsBB6QnI3HdiJOavWxeDFuNii5nKmrHb700HvWOo6gy',
+        'client_secret': 'test-client-secret',
     }
 
     return render(request, template, context)
