0b8666529ce21aedefb80a413784b506a0535d48
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-30 14:32:04 +0000

    add: checkout success view

--- checkout/views.py
+++ checkout/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 from django.core.validators import RegexValidator
 
 from .forms import OrderForm
-from .models import OrderLineItem
+from .models import Order, OrderLineItem
 from products.models import Product
 from bag.contexts import bag_contents
 
@@ -89,3 +89,25 @@ def checkout(request):
         }
 
         return render(request, template, context)
+
+
+def checkout_success(request, order_number):
+    """
+    Handle a successful checkout event
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+    if 'bag' in request.session:
+        del request.session['bag']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+    }
+
+    return render(request, template, context)
