61ee3c11dfd28fa758add5c8b6fad9e5e0fe21d5
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-31 15:31:40 +0000

    add: get the bag, shipping info, save info, charge & grand total then clean shipping data

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -22,8 +22,43 @@ class StripeWH_Handler:
         Handles the payment intent succeeded WebHook from Stripe
         """
         intent = event.data.object
-        print(intent)
+        pid = intent.id
+        bag = intent.metadata.bag
+        save_info = intent.metadata.save_info
+
+        stripe_charge = stripe.Charge.retrieve(intent.latest_charge)
+        billing_details = stripe_charge.billing_details
+        shipping_details = intent.shipping
+        grand_total = round(stripe_charge.amount / 100, 2)
+
+        # clean data from shipping details to prevent empty strings in db
+        for field, value in shipping_details.address.items():
+            if value == "":
+                shipping_details.address[field] = None
+
+        order_exists = False
+        order = Order.objects.get(
+            full_name__iexact=shipping_details.name,
+            email__iexact=billing_details.email,
+            phone_number__iexact=shipping_details.phone,
+            country__iexact=shipping_details.address.country,
+            postcode__iexact=shipping_details.address.postal_code,
+            town_or_city__iexact=shipping_details.address.city,
+            street_address1__iexact=shipping_details.address.line1,
+            street_address2__iexact=shipping_details.address.line2,
+            county__iexact=shipping_details.address.state,
+            grand_total=grand_total,
+        )
+        order_exists = True
         return HttpResponse(
+                    content=f'WebHook received: {event["type"]} | SUCCESS: Verified order already in database',
+                    status=200
+                )
+    except Order.DoesNotExist:
+    
+    
+    
+            return HttpResponse(
             content=f'WebHook received: {event["type"]}',
             status=200
         )
