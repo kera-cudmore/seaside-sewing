fb2353e0ef8433b498b9e373591dc777ada1b755
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-31 20:54:28 +0000

    add: form and orders to the profile view

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
@@ -9,9 +10,13 @@ def profile(request):
     """
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
     }
 
     return render(request, template, context)
