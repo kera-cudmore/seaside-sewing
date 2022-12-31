cf4da6610d0c58b22ed5e9fa902261ddc74afb9a
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-31 22:35:57 +0000

    add: add product url

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name='products'),
-    path('<int:product_id>', views.product_detail, name='product_detail'),
+    path(
+        '',
+        views.all_products,
+        name='products'
+    ),
+    path(
+        '<int:product_id>/',
+        views.product_detail,
+        name='product_detail'
+        ),
+    path(
+        'add/',
+        views.add_product,
+        name='add_product'
+    )
 ]
