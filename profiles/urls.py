9c9acd7d5b98b3acc1b13946c12be547b3ee1a35
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-31 21:33:20 +0000

    add: order history path

--- profiles/urls.py
+++ profiles/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
         views.profile,
         name='profile'
     ),
+    path(
+        'order_history/<order_number>',
+        views.order_history,
+        name='order_history',
+    )
 ]
