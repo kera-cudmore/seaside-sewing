7b3edf8afbec8762e2f7bea8b9b27e1cf7a3ae80
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-31 19:37:53 +0000

    add: import for user profile and passed this to context

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
 
 
 def profile(request):
     """
     Display a users profile Page
     """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
