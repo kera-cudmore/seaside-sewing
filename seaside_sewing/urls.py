673976117213bb6d2e04fd152ffb67df89f97f34
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-31 17:09:31 +0000

    add: profile urls to base app

--- seaside_sewing/urls.py
+++ seaside_sewing/urls.py
@@ -25,6 +25,5 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
-
-    # path('profiles/', include('profiles.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
