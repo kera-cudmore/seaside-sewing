674bd465726e63db6d8946a6d867f948b22f134a
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-31 21:47:23 +0000

    delete: order date from the messages displayed

--- profiles/views.py
+++ profiles/views.py
@@ -37,7 +37,7 @@ def order_history(request, order_number):
 
     messages.info(request, (
         f'This is a past confirmation for order number {order_number}. \
-        A confirmation email was sent on the order date, {order.date}.'
+        A confirmation email was sent on the order date shown below.'
     ))
 
     template = 'checkout/checkout_success.html'
