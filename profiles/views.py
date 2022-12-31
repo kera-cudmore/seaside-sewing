7f234ea6afa45e1d74964e7596f98f43ac852b35
Kera Cudmore <kera.cudmore@hotmail.com>
2022-12-31 21:13:12 +0000

    add: logic to update form

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.conf import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
@@ -9,6 +10,12 @@ def profile(request):
     Display a users profile Page
     """
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile was updated successfully')
 
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
