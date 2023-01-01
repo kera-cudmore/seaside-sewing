b92f3c9d1fd1e298f435a575c6a06f0a16263a9c
Kera Cudmore <kera.cudmore@hotmail.com>
2023-01-01 19:12:55 +0000

    add: if else statement for the databases

--- seaside_sewing/settings.py
+++ seaside_sewing/settings.py
@@ -127,12 +127,17 @@ WSGI_APPLICATION = 'seaside_sewing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
