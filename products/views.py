2a1afa6174f09494f09350b7420a547bc1da6b9c
Kera Cudmore <kera.cudmore@hotmail.com>
2023-01-01 15:05:25 +0000

    add: delete product view

--- products/views.py
+++ products/views.py
@@ -129,3 +129,14 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """
+    Delete Product
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, f'Product {product.name} \
+        has successfully been deleted.')
+    return redirect(reverse('products'))
