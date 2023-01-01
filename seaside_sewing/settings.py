ff292fb605b07fdad9d605fb1ccd041384a1e532
Kera Cudmore <kera.cudmore@hotmail.com>
2023-01-01 20:52:32 +0000

    add: cache control to aws

--- seaside_sewing/settings.py
+++ seaside_sewing/settings.py
@@ -188,6 +188,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache Control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'seaside-sewing'
     AWS_S3_REGION_NAME = 'eu-west-2'
