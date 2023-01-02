a6eb6eeb756e48ba9ee37d0efbef39717be4f294
Kera Cudmore <kera.cudmore@hotmail.com>
2023-01-02 15:31:44 +0000

    add: setup for sending emails on deployed site

--- seaside_sewing/settings.py
+++ seaside_sewing/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', ' ')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
@@ -112,10 +112,7 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-# Allows us to see emails in terminal in development
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-# Email settings for deployed account
+# Account Email settings for deployed account
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
@@ -188,11 +185,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    # Cache Control
+    # Cache Control - tells browser ok to cache for a long time
     AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'seaside-sewing'
     AWS_S3_REGION_NAME = 'eu-west-2'
@@ -228,7 +226,15 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
-# Sending emails
-
-DEFAULT_FROM_EMAIL = 'seaside.sewing.eshop@gmail.com'
-ACCOUNT_EMAIL_VERIFICATION = 'none'
+# SENDING EMAILS
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'seaside.sewing.eshop@gmail.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
