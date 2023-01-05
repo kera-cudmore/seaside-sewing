d78cbc9e559e21b6dfcb5de350d9fc4523e82740
Kera Cudmore <kera.cudmore@hotmail.com>
2023-01-05 17:03:18 +0000

    fix: cast item_id as a string to enable updating item quantity (see solved bug 9)

--- bag/views.py
+++ bag/views.py
@@ -22,11 +22,11 @@ def add_to_bag(request, item_id):
     """
     Add a quantity of the specified product to the shopping bag
     """
-
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
+    item_id = str(item_id)
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
