d34fb39292a547f4695e84f5e355c2b8e233638f
Kera Cudmore <kera.cudmore@hotmail.com>
2023-01-07 21:30:08 +0000

    update: order of fields in admin view

--- checkout/admin.py
+++ checkout/admin.py
@@ -4,18 +4,16 @@ from .models import Order, OrderLineItem
 
 class OrderLineItemAdminInline(admin.TabularInline):
     """
-    Allows us to add and edit line items in the admin
-    right from inside the order model, this will show
-    editable items on the order page
+    Add and edit line items in the admin order
     """
     model = OrderLineItem
     readonly_fields = ('lineitem_total',)
 
 
 class OrderAdmin(admin.ModelAdmin):
-    # These fields are calulated by model methods so we don't want
-    # anyone to have the ability to edit them as it could
-    # compromise the integrety of an order
+    # These fields are calulated by model methods
+    # made readonly so not editable to prevent
+    # order details being compromised
     inlines = (OrderLineItemAdminInline,)
 
     readonly_fields = (
@@ -28,28 +26,25 @@ class OrderAdmin(admin.ModelAdmin):
         'stripe_pid',
     )
 
-    # Not 100% necessary - but will allow us to specify the order of the
-    # fields in the admin interface which would otherwise be adjusted
-    # by Django due to use using some readonly fields -
-    # this allows us to keep the order the same as in the model
+    # Allows us to specify the order of the fields
     fields = (
         'order_number',
-        'user_profile',
         'date',
-        'full_name',
-        'email',
-        'phone_number',
-        'country',
-        'postcode',
-        'town_or_city',
-        'street_address1',
-        'street_address2',
-        'county',
-        'delivery_cost',
+        'user_profile',
         'order_total',
+        'delivery_cost',
         'grand_total',
         'original_bag',
         'stripe_pid',
+        'full_name',
+        'email',
+        'phone_number',
+        'street_address1',
+        'street_address2',
+        'town_or_city',
+        'county',
+        'postcode',
+        'country',
     )
 
     # This restricts the columns that show up in the order list tox
