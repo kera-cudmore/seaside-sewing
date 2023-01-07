7dff17325eb2c7e9a7894e6e62e6b1b4077fb2e4
Kera Cudmore <kera.cudmore@hotmail.com>
2023-01-07 15:39:38 +0000

    Add: stock to the context

--- bag/contexts.py
+++ bag/contexts.py
@@ -19,6 +19,7 @@ def bag_contents(request):
             'item_id': item_id,
             'quantity': quantity,
             'product': product,
+            'stock': product.stock
         })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
