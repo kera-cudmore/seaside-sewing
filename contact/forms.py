e3a2f949c71bf5e316140b26aacb5c2e82f6ecc4
Kera Cudmore <kera.cudmore@hotmail.com>
2023-01-08 14:27:49 +0000

    update: docstring

--- contact/forms.py
+++ contact/forms.py
@@ -14,8 +14,9 @@ class ContactForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         """
-        Add placeholders and classes, remove auto-generated
-        labels and set autofocus on first field
+        Add placeholders,
+        remove auto-generated labels
+        set autofocus on first field
         """
         super().__init__(*args, **kwargs)
         placeholders = {
