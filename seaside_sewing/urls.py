a887ec249323b0cea27a0b5d611f5c3839707ca2
Kera Cudmore <kera.cudmore@hotmail.com>
2023-01-08 14:05:05 +0000

    registered contact app urls

--- seaside_sewing/urls.py
+++ seaside_sewing/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+    path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
