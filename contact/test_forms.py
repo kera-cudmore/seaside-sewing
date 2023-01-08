934aa2d86c9c91a0d1fe2f68a3039c8137432e31
Kera Cudmore <kera.cudmore@hotmail.com>
2023-01-08 20:01:20 +0000

    add: test to check email is required

--- contact/test_forms.py
+++ contact/test_forms.py
@@ -10,3 +10,11 @@ class TestContactForm(TestCase):
         self.assertIn('contact_name', form.errors.keys())
         self.assertEqual(
             form.errors['contact_name'][0], 'This field is required.')
+
+
+    def test_contactform_contact_email_is_required(self):
+        form = ContactForm({'contact_email': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('contact_email', form.errors.keys())
+        self.assertEqual(
+            form.errors['contact_email'][0], 'This field is required.')
