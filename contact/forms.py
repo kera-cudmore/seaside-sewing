a4dabf0bfbe306600c7419e51acfe35d3c6dabf6
Kera Cudmore <kera.cudmore@hotmail.com>
2023-01-08 14:10:22 +0000

    created: contact form and its placeholders & autofocus added to name

---
+++ contact/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from .models import ContactForm
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = ContactForm
+        fields = (
+            'contact_name',
+            'contact_email',
+            'contact_phone_number',
+            'contact_message',
+        )
+
+    def __init__(self, *args, **kwargs):
+            """
+            Add placeholders and classes, remove auto-generated
+            labels and set autofocus on first field
+            """
+            super().__init__(*args, **kwargs)
+            placeholders = {
+                'contact_name': 'Full Name',
+                'contact_email': 'Email Address',
+                'contact_phone_number': 'Phone Number',
+                'contact_message': 'Your Message',
+            }
+
+            self.fields['contact_name'].widget.attrs['autofocus'] = True
+
+            for field in self.fields:
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+                self.fields[field].label = False
