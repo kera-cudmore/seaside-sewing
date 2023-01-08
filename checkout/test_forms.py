68068c4945f4a524d73b828523a67be662db5c3e
Kera Cudmore <kera.cudmore@hotmail.com>
2023-01-08 01:08:08 +0000

    add: test email is required

--- checkout/test_forms.py
+++ checkout/test_forms.py
@@ -10,3 +10,14 @@ class TestOrderForm(TestCase):
         self.assertIn('full_name', form.errors.keys())
         self.assertEqual(
             form.errors['full_name'][0], 'This field is required.')
+
+    def test_orderform_email_is_required(self):
+        form = OrderForm(
+            {
+                'full_name': 'A Name',
+                'email': '',
+            })
+        self.assertFalse(form.is_valid())
+        self.assertIn('email', form.errors.keys())
+        self.assertEqual(
+            form.errors['email'][0], 'This field is required.')
