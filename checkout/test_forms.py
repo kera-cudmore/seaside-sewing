f941decfa9125bc44d8496ecb4fcfa9cfbf74910
Kera Cudmore <kera.cudmore@hotmail.com>
2023-01-08 01:03:36 +0000

    add: test full_name is required

---
+++ checkout/test_forms.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from .forms import OrderForm
+
+
+class TestOrderForm(TestCase):
+
+    def test_orderform_full_name_is_required(self):
+        form = OrderForm({'full_name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('full_name', form.errors.keys())
+        self.assertEqual(
+            form.errors['full_name'][0], 'This field is required.')
