18548c1258c8b3ccc2ba5b56dea7d44da0d91adf
Kera Cudmore <kera.cudmore@hotmail.com>
2023-01-08 18:16:31 +0000

    add: view for contact form

--- contact/views.py
+++ contact/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ContactForm
-from .models import ContactForm
+# from .models import ContactForm
 
 from django.contrib import messages
-from django.conf import settings
+# from django.conf import settings
 
 
 def contact(request):
-    
-    return render(request, 'contact/contact.html')
+
+    if request.method == 'POST':
+        contact_form = ContactForm(request.POST)
+        if contact_form.is_valid():
+            contact_form.save()
+            messages.success(request, 'Your enquiry was sent successfully. \
+                Please allow up to 2 working days for a response.')
+            return render(request, 'contact/contact_success.html')
+        else:
+            messages.error(request, 'There was an error sending your enquiry. \
+            Please ensure all fields are valid and try again.')
+            return redirect('contact')
+    else:
+        contact_form = ContactForm()
+
+    template = 'contact/contact.html'
+    context = {
+        'form': contact_form,
+    }
+
+    return render(request, template, context)
