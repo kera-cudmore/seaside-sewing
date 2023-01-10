fb91616412036e78b63d731e58baa39e8a2a0270
Kera Cudmore <kera.cudmore@hotmail.com>
2023-01-10 20:18:34 +0000

    add: test to check contact returns 200 status and uses contact template

---
+++ contact/test_views.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+
+class TestContactViews(TestCase):
+
+    def test_get_contact_page(self):
+        response = self.client.get('/contact/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contact/contact.html')
