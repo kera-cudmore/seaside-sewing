14d30ab35e0477e4f10a905f182dfe10d280c91d
Kera Cudmore <kera.cudmore@hotmail.com>
2023-01-10 19:03:32 +0000

    add: test home page view returns 200 status & correct template

---
+++ home/test_views.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+
+class TestHomeViews(TestCase):
+
+    def test_get_home_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
