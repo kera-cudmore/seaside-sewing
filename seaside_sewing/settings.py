e50fbd906a70c760e4b872b4147b1645b07d147f
Kera Cudmore <kera.cudmore@hotmail.com>
2023-01-10 11:21:31 +0000

    add: email account verification key to prevent 500 error during log in on live site

--- seaside_sewing/settings.py
+++ seaside_sewing/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', ' ')
 
 DEBUG = 'DEVELOPMENT' in os.environ
-# THIS IS JUST TO SEE CSS ETC IN DEVELOPMENT - REMOVE THIS before sub
-# DEBUG = True
 
 ALLOWED_HOSTS = ['seaside-sewing.herokuapp.com', 'localhost']
 
@@ -224,3 +222,5 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
