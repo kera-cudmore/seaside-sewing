72df61db0c58b2dab286bc3b0591a752340d6747
Kera Cudmore <kera.cudmore@hotmail.com>
2023-01-13 20:25:54 +0000

    add: test admin get add product page

--- products/test_views.py
+++ products/test_views.py
@@ -1,4 +1,7 @@
 from django.test import TestCase
+from .models import Category, Product
+from django.contrib.auth.models import User
+from django.test.client import Client
 
 
 class TestProductViews(TestCase):
@@ -7,3 +10,16 @@ class TestProductViews(TestCase):
         response = self.client.get('/products/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/products.html')
+
+    def test_get_add_product_page_admin(self):
+        password = 'mypassword'
+        superuser = User.objects.create_superuser(
+            'superuser',
+            'myemail@test.com',
+            password
+        )
+        # You'll need to log in before you can send requests through the client
+        self.client.login(username=superuser.username, password=password)
+        response = self.client.get('/products/add/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed((response, 'products/add_product.html'))
