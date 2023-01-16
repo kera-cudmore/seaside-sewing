9a761a00fba171e119fbc5f7383f1a68e141a6f2
Kera Cudmore <kera.cudmore@hotmail.com>
2023-01-16 12:30:13 +0000

    add: test user not logged in 404

--- profiles/test_views.py
+++ profiles/test_views.py
@@ -4,6 +4,10 @@ from django.urls import reverse
 
 class TestProfilesViews(TestCase):
 
+    def test_redirect_if_user_not_logged_in(self):
+        response = self.client.get('profile')
+        self.assertEqual(response.status_code, 404)
+
     def test_logged_in_user_can_access_profile(self):
         login = self.client.login(
             username='testuser1',
