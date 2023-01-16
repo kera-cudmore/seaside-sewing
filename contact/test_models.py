abb091ec0cb9078b36b9c36cfae5060e4ffc077c
Kera Cudmore <kera.cudmore@hotmail.com>
2023-01-16 11:29:45 +0000

    add: test to check replied is default false

--- contact/test_models.py
+++ contact/test_models.py
@@ -7,3 +7,12 @@ class TestContactModels(TestCase):
     def test_string_representation(self):
         contact = ContactForm(contact_email="test@email.com")
         self.assertEqual(str(contact), contact.contact_email)
+
+    def test_replied_defaults_to_false(self):
+        contact_form = ContactForm.objects.create(
+            contact_name="test name",
+            contact_email="test@test.com",
+            contact_phone_number="123345",
+            contact_message="this is a test message",
+        )
+        self.assertFalse(contact_form.replied)
