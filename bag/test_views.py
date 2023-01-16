7f70c862bf321a54f68e080025cbb893cce22333
Kera Cudmore <kera.cudmore@hotmail.com>
2023-01-16 17:40:19 +0000

    add: test to add product to bag

--- bag/test_views.py
+++ bag/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from products.models import Product
 
 
 class TestBagViews(TestCase):
@@ -7,3 +8,21 @@ class TestBagViews(TestCase):
         response = self.client.get('/bag/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'bag/bag.html')
+
+    def test_add_product_to_bag(self):
+        product = Product.objects.create(
+            sku='12334',
+            name='Test Product',
+            description='This is the description for a test product',
+            price=12.99,
+            stock=3,
+            rating=3,
+            colour='Blue',
+        )
+
+        response = self.client.post(
+            f'/bag/add/{product.id}/',
+            {'quantity': 1, 'redirect_url': 'view_bag'}
+        )
+        bag = self.client.session['bag']
+        self.assertEqual(bag[str(product.id)], 1)
