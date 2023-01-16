b3349d97c95805f88c712da396957f7849c43f5e
Kera Cudmore <kera.cudmore@hotmail.com>
2023-01-16 11:11:14 +0000

    add: product model unit test - pluralise category

--- products/test_models.py
+++ products/test_models.py
@@ -7,3 +7,6 @@ class TestProductModels(TestCase):
     def test_string_representation(self):
         product = Product(name="Test Product Name")
         self.assertEqual(str(product), product.name)
+
+    def test_category_verbose_name_plural(self):
+        self.assertEqual(str(Category._meta.verbose_name_plural), "Categories")
