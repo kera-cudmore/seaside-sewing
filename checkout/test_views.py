d15bcf4abf4a2861cd98b5f9fe7abebbde12da15
Kera Cudmore <kera.cudmore@hotmail.com>
2023-01-16 13:22:12 +0000

    rename: test

--- checkout/test_views.py
+++ checkout/test_views.py
@@ -3,8 +3,7 @@ from django.test import TestCase
 
 class TestCheckoutViews(TestCase):
 
-    def test_get_checkout_page_empty_bag(self):
-        bag = {}
+    def test_get_checkout_with_empty_bag(self):
 
-        response = self.client.get('/checkout/', )
-        self.assertEqual(response.status_code, 302)
+        response = self.client.get('/checkout')
+        self.assertTrue(response, '/products')
