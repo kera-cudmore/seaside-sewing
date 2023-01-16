19e2e4afa40e108e10a0a5e01ebc4cc82236f91a
Kera Cudmore <kera.cudmore@hotmail.com>
2023-01-16 16:37:16 +0000

    remove: unrequired imports

--- profiles/test_models.py
+++ profiles/test_models.py
@@ -1,7 +1,5 @@
 from django.test import TestCase
-from django.urls import reverse
 from django.contrib.auth.models import User
-from .models import UserProfile
 
 
 class TestProfilesModels(TestCase):
