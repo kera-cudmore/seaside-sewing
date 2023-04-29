1438b52c9693894428a4195a5fcba82f8c57a0bf
Kera Cudmore <kera.cudmore@hotmail.com>
2023-04-29 11:25:24 +0100

    remove: https from front of allowed host

--- seaside_sewing/settings.py
+++ seaside_sewing/settings.py
@@ -26,12 +26,12 @@ DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     'seaside-sewing.herokuapp.com',
-    'https://web-production-e39f.up.railway.app',
+    'web-production-e39f.up.railway.app',
     'localhost',
     '127.0.0.1'
     ]
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-e39f.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['web-production-e39f.up.railway.app']
 
 # Application definition
 INSTALLED_APPS = [
