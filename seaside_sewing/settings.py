eeb50c26fa809b98df0380c3c87d80087cc70697
Kera Cudmore <kera.cudmore@hotmail.com>
2023-04-29 10:59:20 +0100

    revert: changes using django-environ as deployment failing

--- seaside_sewing/settings.py
+++ seaside_sewing/settings.py
@@ -8,38 +8,29 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+if os.path.exists('env.py'):
+    import env  # noqa: F401
+
 import dj_database_url
+
 from pathlib import Path
 import decimal
 from decimal import Decimal
 
-import environ
-
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+SECRET_KEY = os.environ.get('SECRET_KEY', ' ')
 
-
-SECRET_KEY = env('SECRET_KEY')
-
-# False if not in os.environ because of casting above
-DEBUG = env('DEVELOPMENT')
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
-    'web-production-09dc.up.railway.app/',
     'seaside-sewing.herokuapp.com',
-    'localhost',
-    '127.0.0.1'
-]
+    'web-production-09dc.up.railway.app/',
+    'localhost', '127.0.0.1'
+    ]
 
-# CSRF_TRUSTED_ORIGINS = ['https://web-production-09dc.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['web-production-09dc.up.railway.app/']
 
 # Application definition
 INSTALLED_APPS = [
@@ -136,7 +127,7 @@ WSGI_APPLICATION = 'seaside_sewing.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(env('DATABASE_URL'))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
@@ -209,8 +200,8 @@ if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'seaside-sewing'
     AWS_S3_REGION_NAME = 'eu-west-2'
-    AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and Media Files
@@ -249,6 +240,6 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = env('EMAIL_HOST_USER')
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
