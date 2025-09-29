560681705fc077ad46f696c3c79cc2e03d8753da
Kera Cudmore <kera.cudmore@hotmail.com>
2025-09-29 18:27:42 +0100

    update: static_root

--- seaside_sewing/settings.py
+++ seaside_sewing/settings.py
@@ -200,7 +200,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles_collected'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Media files
